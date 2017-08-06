7167764c5b1bcf49af95c0fea98f60f4f31c4071
Denny <932755233@github.com>
2017-08-07 01:17:13 +0800

    添加移动文件的C#程序

---
+++ MoveFile/MoveFile/Form1.Designer.cs
@@ -0,0 +1,261 @@
+﻿namespace MoveFile
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbxInput = new System.Windows.Forms.TextBox();
+            this.btnInput = new System.Windows.Forms.Button();
+            this.btnOutput = new System.Windows.Forms.Button();
+            this.tbxOutput = new System.Windows.Forms.TextBox();
+            this.pbxPhoto = new System.Windows.Forms.PictureBox();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnMove = new System.Windows.Forms.Button();
+            this.fbdDirectory = new System.Windows.Forms.FolderBrowserDialog();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.菜单ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.msInput = new System.Windows.Forms.ToolStripMenuItem();
+            this.msOutput = new System.Windows.Forms.ToolStripMenuItem();
+            this.msSaveSetting = new System.Windows.Forms.ToolStripMenuItem();
+            this.msExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.tvwDirectories = new System.Windows.Forms.TreeView();
+            this.lvwFiles = new System.Windows.Forms.ListView();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxPhoto)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tbxInput
+            // 
+            this.tbxInput.Location = new System.Drawing.Point(12, 29);
+            this.tbxInput.Name = "tbxInput";
+            this.tbxInput.Size = new System.Drawing.Size(428, 21);
+            this.tbxInput.TabIndex = 0;
+            this.tbxInput.TextChanged += new System.EventHandler(this.tbxInput_TextChanged);
+            // 
+            // btnInput
+            // 
+            this.btnInput.Location = new System.Drawing.Point(446, 29);
+            this.btnInput.Name = "btnInput";
+            this.btnInput.Size = new System.Drawing.Size(135, 23);
+            this.btnInput.TabIndex = 1;
+            this.btnInput.Text = "选择输入文件夹";
+            this.btnInput.UseVisualStyleBackColor = true;
+            this.btnInput.Click += new System.EventHandler(this.btnInput_Click);
+            // 
+            // btnOutput
+            // 
+            this.btnOutput.Location = new System.Drawing.Point(446, 56);
+            this.btnOutput.Name = "btnOutput";
+            this.btnOutput.Size = new System.Drawing.Size(135, 23);
+            this.btnOutput.TabIndex = 3;
+            this.btnOutput.Text = "选择输出文件夹";
+            this.btnOutput.UseVisualStyleBackColor = true;
+            this.btnOutput.Click += new System.EventHandler(this.btnOutput_Click);
+            // 
+            // tbxOutput
+            // 
+            this.tbxOutput.Location = new System.Drawing.Point(12, 56);
+            this.tbxOutput.Name = "tbxOutput";
+            this.tbxOutput.Size = new System.Drawing.Size(428, 21);
+            this.tbxOutput.TabIndex = 2;
+            this.tbxOutput.TextChanged += new System.EventHandler(this.tbxOutput_TextChanged);
+            // 
+            // pbxPhoto
+            // 
+            this.pbxPhoto.Location = new System.Drawing.Point(12, 304);
+            this.pbxPhoto.Name = "pbxPhoto";
+            this.pbxPhoto.Size = new System.Drawing.Size(569, 491);
+            this.pbxPhoto.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbxPhoto.TabIndex = 4;
+            this.pbxPhoto.TabStop = false;
+            this.pbxPhoto.DoubleClick += new System.EventHandler(this.pbxPhoto_DoubleClick);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(12, 801);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(168, 23);
+            this.btnDelete.TabIndex = 5;
+            this.btnDelete.Text = "删除";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // btnMove
+            // 
+            this.btnMove.Location = new System.Drawing.Point(414, 801);
+            this.btnMove.Name = "btnMove";
+            this.btnMove.Size = new System.Drawing.Size(168, 23);
+            this.btnMove.TabIndex = 6;
+            this.btnMove.Text = "移动";
+            this.btnMove.UseVisualStyleBackColor = true;
+            // 
+            // fbdDirectory
+            // 
+            this.fbdDirectory.HelpRequest += new System.EventHandler(this.fbdDirectory_HelpRequest);
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Location = new System.Drawing.Point(12, 85);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.tvwDirectories);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.lvwFiles);
+            this.splitContainer1.Size = new System.Drawing.Size(569, 213);
+            this.splitContainer1.SplitterDistance = 125;
+            this.splitContainer1.TabIndex = 7;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.菜单ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(596, 25);
+            this.menuStrip1.TabIndex = 8;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 菜单ToolStripMenuItem
+            // 
+            this.菜单ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.msInput,
+            this.msOutput,
+            this.msSaveSetting,
+            this.msExit});
+            this.菜单ToolStripMenuItem.Name = "菜单ToolStripMenuItem";
+            this.菜单ToolStripMenuItem.Size = new System.Drawing.Size(44, 21);
+            this.菜单ToolStripMenuItem.Text = "菜单";
+            // 
+            // msInput
+            // 
+            this.msInput.Name = "msInput";
+            this.msInput.Size = new System.Drawing.Size(160, 22);
+            this.msInput.Text = "选择输入文件夹";
+            this.msInput.Click += new System.EventHandler(this.msInput_Click);
+            // 
+            // msOutput
+            // 
+            this.msOutput.Name = "msOutput";
+            this.msOutput.Size = new System.Drawing.Size(160, 22);
+            this.msOutput.Text = "选择输出文件夹";
+            this.msOutput.Click += new System.EventHandler(this.msOutput_Click);
+            // 
+            // msSaveSetting
+            // 
+            this.msSaveSetting.Name = "msSaveSetting";
+            this.msSaveSetting.Size = new System.Drawing.Size(160, 22);
+            this.msSaveSetting.Text = "保存配置";
+            this.msSaveSetting.Click += new System.EventHandler(this.msSaveSetting_Click);
+            // 
+            // msExit
+            // 
+            this.msExit.Name = "msExit";
+            this.msExit.Size = new System.Drawing.Size(160, 22);
+            this.msExit.Text = "退出";
+            this.msExit.Click += new System.EventHandler(this.msExit_Click);
+            // 
+            // tvwDirectories
+            // 
+            this.tvwDirectories.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tvwDirectories.Location = new System.Drawing.Point(0, 0);
+            this.tvwDirectories.Name = "tvwDirectories";
+            this.tvwDirectories.Size = new System.Drawing.Size(125, 213);
+            this.tvwDirectories.TabIndex = 0;
+            this.tvwDirectories.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.tvwDirectories_AfterSelect);
+            // 
+            // lvwFiles
+            // 
+            this.lvwFiles.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lvwFiles.Location = new System.Drawing.Point(0, 0);
+            this.lvwFiles.Name = "lvwFiles";
+            this.lvwFiles.Size = new System.Drawing.Size(440, 213);
+            this.lvwFiles.TabIndex = 0;
+            this.lvwFiles.UseCompatibleStateImageBehavior = false;
+            this.lvwFiles.View = System.Windows.Forms.View.Details;
+            this.lvwFiles.SelectedIndexChanged += new System.EventHandler(this.lvwFiles_SelectedIndexChanged);
+            this.lvwFiles.Click += new System.EventHandler(this.lvwFiles_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(596, 887);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.btnMove);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.pbxPhoto);
+            this.Controls.Add(this.btnOutput);
+            this.Controls.Add(this.tbxOutput);
+            this.Controls.Add(this.btnInput);
+            this.Controls.Add(this.tbxInput);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbxPhoto)).EndInit();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbxInput;
+        private System.Windows.Forms.Button btnInput;
+        private System.Windows.Forms.Button btnOutput;
+        private System.Windows.Forms.TextBox tbxOutput;
+        private System.Windows.Forms.PictureBox pbxPhoto;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnMove;
+        private System.Windows.Forms.FolderBrowserDialog fbdDirectory;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 菜单ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem msInput;
+        private System.Windows.Forms.ToolStripMenuItem msOutput;
+        private System.Windows.Forms.ToolStripMenuItem msSaveSetting;
+        private System.Windows.Forms.ToolStripMenuItem msExit;
+        private System.Windows.Forms.TreeView tvwDirectories;
+        private System.Windows.Forms.ListView lvwFiles;
+    }
+}
+
---
+++ MoveFile/MoveFile/Form1.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MoveFile
+{
+    public partial class Form1 : Form
+    {
+
+        string[] extension = { ".png", ".jpg", ".gif", ".jpeg" };
+
+        //第几个图片
+        int index = -1;
+
+        string inputStr = @"G:\QQ文档\932755233\Image\C2C";
+        string outputStr = "G:\\QQ文档\\932755233\\Image\\C2C";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            tbxInput.Text = inputStr;
+            lvwFiles.Columns.Add("文件名", 200);
+            lvwFiles.Columns.Add("大小", 100);
+            lvwFiles.Columns.Add("时间", 100);
+
+            lvwFiles.View = View.Details;
+        }
+
+        private void btnInput_Click(object sender, EventArgs e)
+        {
+
+            if (!"".Equals(inputStr))
+            {
+                fbdDirectory.SelectedPath = inputStr;
+            }
+            fbdDirectory.Description = "请选择一个输入文件夹";
+            fbdDirectory.ShowNewFolderButton = false;
+            if (fbdDirectory.ShowDialog() == DialogResult.OK)
+            {
+                inputStr = fbdDirectory.SelectedPath;
+                tbxInput.Text = fbdDirectory.SelectedPath;
+                openInput();
+            }
+
+
+
+        }
+
+        private void btnOutput_Click(object sender, EventArgs e)
+        {
+            //
+            if (!"".Equals(outputStr))
+            {
+                fbdDirectory.SelectedPath = tbxInput.Text = inputStr;
+            }
+            fbdDirectory.Description = "请选择一个输出文件夹";
+            fbdDirectory.ShowNewFolderButton = true;
+            if (fbdDirectory.ShowDialog() == DialogResult.OK)
+            {
+                outputStr = fbdDirectory.SelectedPath;
+                tbxOutput.Text = fbdDirectory.SelectedPath;
+
+
+
+
+
+            }
+        }
+
+        private void fbdDirectory_HelpRequest(object sender, EventArgs e)
+        {
+        }
+
+        private void openInput()
+        {
+
+
+            DirectoryInfo TheFolder = new DirectoryInfo(inputStr);
+            Console.WriteLine(TheFolder.GetFiles().Length);
+            foreach (FileInfo NextFolder in TheFolder.GetFiles())
+            {
+                Console.WriteLine("---------------------------------");
+                Console.WriteLine("DirectoryName\t{0}", NextFolder.DirectoryName);//所在的文件夹
+                Console.WriteLine("Extension\t{0}", NextFolder.Extension);//扩展名
+                Console.WriteLine("FullName\t{0}", NextFolder.FullName);//文件路径加文件名
+                Console.WriteLine("Name\t{0}", NextFolder.Name);//文件名
+                Console.WriteLine("---------------------------------");
+
+                var item = new ListViewItem(NextFolder.Name);
+                item.SubItems.Add(NextFolder.Length.ToString());
+                item.SubItems.Add(NextFolder.LastWriteTime.ToLocalTime().ToString("yyyy-MM-dd HH:mm:ss"));
+                item.Tag = NextFolder.FullName;
+                lvwFiles.Items.Add(item);
+
+
+            }
+
+
+
+        }
+
+        private void msInput_Click(object sender, EventArgs e)
+        {
+            openInput();
+        }
+
+        private void msOutput_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void msSaveSetting_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void msExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void tbxInput_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbxOutput_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tvwDirectories_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+
+        }
+
+        private void lvwFiles_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (lvwFiles.SelectedItems.Count > 0)
+            {
+                pbxPhoto.ImageLocation = lvwFiles.SelectedItems[0].Tag.ToString();
+            }
+        }
+
+        private void lvwFiles_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pbxPhoto_DoubleClick(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start(pbxPhoto.ImageLocation);
+        }
+    }
+}
